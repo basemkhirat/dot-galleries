3a60cd8acc0f080a1bf2d47d654b25dffb1f2ed0
basemkhirat <basemkhirat@gmail.com>
2017-10-06 18:22:46 +0200

    fixes

--- src/routes.php
+++ src/routes.php
@@ -1,34 +1,39 @@
 <?php
 
-Route::group(array(
+/**
+ * WEB
+ */
+
+Route::group([
     "prefix" => ADMIN,
-    "middleware" => ["web", "auth"],
-), function ($route) {
-    $route->group(array("prefix" => "galleries"), function ($route) {
-
-        $route->any('/delete', array("as" => "admin.galleries.delete", "uses" => "Dot\Galleries\Controllers\GalleriesController@delete"));
-        $route->any('/save', array("as" => "admin.galleries.save", "uses" => "Dot\Galleries\Controllers\GalleriesController@save"));
-        $route->any('/files', array("as" => "admin.galleries.files", "uses" => "Dot\Galleries\Controllers\GalleriesController@files"));
-        $route->any('/create', array("as" => "admin.galleries.create", "uses" => "Dot\Galleries\Controllers\GalleriesController@create"));
-        $route->any('/{id?}', array("as" => "admin.galleries.show", "uses" => "Dot\Galleries\Controllers\GalleriesController@index"));
-
-        $route->any('/content', array("as" => "admin.galleries.content", "uses" => "Dot\Galleries\Controllers\GalleriesController@content"));
-        $route->any('/{id}/edit', array("as" => "admin.galleries.edit", "uses" => "Dot\Galleries\Controllers\GalleriesController@edit"));
-        $route->any('/get/{offset?}', array("as" => "admin.galleries.ajax", "uses" => "Dot\Galleries\Controllers\GalleriesController@get"));
+    "middleware" => ["web", "auth:backend"],
+    "namespace" => "Dot\\Galleries\\Controllers"
+], function ($route) {
+    $route->group(["prefix" => "galleries"], function ($route) {
+        $route->any('/delete', ["as" => "admin.galleries.delete", "uses" => "GalleriesController@delete"]);
+        $route->any('/save', ["as" => "admin.galleries.save", "uses" => "GalleriesController@save"]);
+        $route->any('/files', ["as" => "admin.galleries.files", "uses" => "GalleriesController@files"]);
+        $route->any('/create', ["as" => "admin.galleries.create", "uses" => "GalleriesController@create"]);
+        $route->any('/{id?}', ["as" => "admin.galleries.show", "uses" => "GalleriesController@index"]);
+        $route->any('/content', ["as" => "admin.galleries.content", "uses" => "GalleriesController@content"]);
+        $route->any('/{id}/edit', ["as" => "admin.galleries.edit", "uses" => "GalleriesController@edit"]);
+        $route->any('/get/{offset?}', ["as" => "admin.galleries.ajax", "uses" => "GalleriesController@get"]);
     });
 });
 
 /*
  * API
  */
+
 Route::group([
     "prefix" => API,
-    "middleware" => ["auth:api"]
+    "middleware" => ["auth:api"],
+    "namespace" => "Dot\\Galleries\\Controllers"
 ], function ($route) {
-    $route->get("/galleries/show", "Dot\Galleries\Controllers\GalleriesApiController@show");
-    $route->post("/galleries/create", "Dot\Galleries\Controllers\GalleriesApiController@create");
-    $route->post("/galleries/update", "Dot\Galleries\Controllers\GalleriesApiController@update");
-    $route->post("/galleries/destroy", "Dot\Galleries\Controllers\GalleriesApiController@destroy");
+    $route->get("/galleries/show", "GalleriesApiController@show");
+    $route->post("/galleries/create", "GalleriesApiController@create");
+    $route->post("/galleries/update", "GalleriesApiController@update");
+    $route->post("/galleries/destroy", "GalleriesApiController@destroy");
 });
 
 
