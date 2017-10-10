59752c260f03d781dd9d5e5e1ae002fcce818869
basemkhirat <basemkhirat@gmail.com>
2017-10-10 17:11:49 +0200

    bladize_some_views

--- src/views/show.blade.php
+++ src/views/show.blade.php
@@ -24,7 +24,7 @@
                 <div class="input-group">
                     <input name="q" value="{{ Request::get("q") }}" type="text" class=" form-control"
                            placeholder="{{ trans("galleries::galleries.search_galleries") }} ..."> <span
-                        class="input-group-btn">
+                            class="input-group-btn">
                     <button class="btn btn-primary"
                             type="button"> <i class="fa fa-search"></i> </button> </span>
                 </div>
@@ -33,7 +33,7 @@
 
         <div class="col-lg-2 pull-right">
             <a href="{{ route("admin.galleries.create") }}" class="btn btn-primary btn-labeled btn-main"> <span
-                    class="btn-label icon fa fa-plus"></span> {{ trans("galleries::galleries.add_new") }}
+                        class="btn-label icon fa fa-plus"></span> {{ trans("galleries::galleries.add_new") }}
             </a>
         </div>
     </div>
@@ -54,98 +54,97 @@
 
                             @if (count($galleries))
 
-                            <div class="row">
-                                <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 action-box">
+                                <div class="row">
+                                    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12 action-box">
 
-                                    <select name="action" class="form-control pull-left">
-                                        <option value="-1"
-                                                selected="selected">{{ trans("galleries::galleries.bulk_actions") }}</option>
-                                        <option
-                                            value="delete">{{ trans("galleries::galleries.delete") }}</option>
-                                    </select>
-                                    <button type="submit"
-                                            class="btn btn-primary pull-right">{{ trans("galleries::galleries.apply") }}</button>
+                                        <select name="action" class="form-control pull-left">
+                                            <option value="-1"
+                                                    selected="selected">{{ trans("galleries::galleries.bulk_actions") }}</option>
+                                            <option
+                                                    value="delete">{{ trans("galleries::galleries.delete") }}</option>
+                                        </select>
+                                        <button type="submit"
+                                                class="btn btn-primary pull-right">{{ trans("galleries::galleries.apply") }}</button>
 
+                                    </div>
+                                    <div class="col-lg-6 col-md-4 hidden-sm hidden-xs">
+
+                                    </div>
+
+                                    <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12">
+
+                                        <select class="form-control per_page_filter">
+                                            <option value="" selected="selected">
+                                                -- {{ trans("galleries::galleries.per_page") }} --
+                                            </option>
+                                            @foreach (array(10, 20, 30, 40) as $num)
+                                                <option
+                                                        value="{{ $num }}"
+                                                        @if ($num == $per_page) selected="selected" @endif>{{ $num }}</option>
+                                            @endforeach
+                                        </select>
+
+                                    </div>
                                 </div>
-                                <div class="col-lg-6 col-md-4 hidden-sm hidden-xs">
+                                <div class="table-responsive">
 
-                                </div>
-
-                                <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12">
-
-                                    <select class="form-control per_page_filter">
-                                        <option value="" selected="selected">
-                                            -- {{ trans("galleries::galleries.per_page") }} --
-                                        </option>
-                                        @foreach (array(10, 20, 30, 40) as $num)
-                                        <option
-                                            value="{{ $num }}"
-                                            @if ($num == $per_page) selected="selected" @endif>{{ $num }}</option>
+                                    <table cellpadding="0" cellspacing="0" border="0" class="table table-striped"
+                                           id="jq-datatables-example">
+                                        <thead>
+                                        <tr>
+                                            <th style="width:35px"><input type="checkbox" class="check_all i-checks"
+                                                                          name="ids[]"/></th>
+                                            <th>{{ trans("galleries::galleries.name") }}</th>
+                                            <th>{{ trans("galleries::galleries.media_count") }}</th>
+                                            <th>{{ trans("galleries::galleries.actions") }}</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach ($galleries as $gallery)
+                                            <tr>
+                                                <td>
+                                                    <input class="i-checks" type="checkbox" name="id[]"
+                                                           value="{{ $gallery->id }}"/>
+                                                </td>
+                                                <td>
+                                                    <a class="text-navy"
+                                                       href="{{ route("admin.galleries.edit", ["id" => $gallery->id]) }}">
+                                                        <strong>{{ $gallery->name }}</strong>
+                                                    </a>
+                                                </td>
+                                                <td>
+                                                    <small>{{ $gallery->files()->count() }}</small>
+                                                </td>
+                                                <td class="center">
+                                                    <a href="{{ route("admin.galleries.edit", ["id" => $gallery->id]) }}">
+                                                        <i class="fa fa-pencil text-navy"></i>
+                                                    </a>
+                                                    <a class="ask"
+                                                       message="{{ trans("galleries::galleries.sure_delete") }}"
+                                                       href="{{ route("admin.galleries.delete", ["id" => $gallery->id]) }}">
+                                                        <i class="fa fa-times text-navy"></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
                                         @endforeach
-                                    </select>
+
+                                        </tbody>
+                                    </table>
 
                                 </div>
-                            </div>
-                            <div class="table-responsive">
-
-                                <table cellpadding="0" cellspacing="0" border="0" class="table table-striped"
-                                       id="jq-datatables-example">
-                                    <thead>
-                                    <tr>
-                                        <th style="width:35px"><input type="checkbox" class="check_all i-checks"
-                                                                      name="ids[]"/></th>
-                                        <th>{{ trans("galleries::galleries.name") }}</th>
-                                        <th>{{ trans("galleries::galleries.media_count") }}</th>
-                                        <th>{{ trans("galleries::galleries.actions") }}</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach ($galleries as $gallery) 
-                                    <tr>
-                                        <td>
-                                            <input class="i-checks" type="checkbox" name="id[]"
-                                                   value="{{ $gallery->id }}"/>
-                                        </td>
-                                        <td>
-                                            <a class="text-navy"
-                                               href="{{ route("admin.galleries.edit", ["id" => $gallery->id]) }}">
-                                                <strong>{{ $gallery->name }}</strong>
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <small>{{ $gallery->files()->count() }}</small>
-
-                                        </td>
-                                        <td class="center">
-                                            <a href="{{ URL::to(ADMIN) }}/galleries/{{ $gallery->id }}/edit">
-                                                <i class="fa fa-pencil text-navy"></i>
-                                            </a>
-                                            <a class="ask"
-                                               message="{{ trans("galleries::galleries.sure_delete") }}"
-                                               href="{{ URL::to(ADMIN) }}/galleries/delete?id[]={{ $gallery->id }}">
-                                                <i class="fa fa-times text-navy"></i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-
-                                    </tbody>
-                                </table>
-
-                            </div>
 
 
-                            <div class="row">
-                                <div class="col-lg-12 text-center">
-                                    {{ trans("galleries::galleries.page") }}
-                                    {{ $galleries->currentPage() }}
-                                    {{ trans("galleries::galleries.of") }}
-                                    {{ $galleries->lastPage() }}
+                                <div class="row">
+                                    <div class="col-lg-12 text-center">
+                                        {{ trans("galleries::galleries.page") }}
+                                        {{ $galleries->currentPage() }}
+                                        {{ trans("galleries::galleries.of") }}
+                                        {{ $galleries->lastPage() }}
+                                    </div>
+                                    <div class="col-lg-12 text-center">
+                                        {{ $galleries->appends(Request::all())->render() }}
+                                    </div>
                                 </div>
-                                <div class="col-lg-12 text-center">
-                                    {{ $galleries->appends(Request::all())->render() }}
-                                </div>
-                            </div>
 
                             @else
                                 {{ trans("galleries::galleries.no_galleries") }}
